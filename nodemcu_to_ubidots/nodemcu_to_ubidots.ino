998659c781937faea49f65d01431086e6b33dea1
Daniel Woodson <s.dwoodson@gmail.com>
2020-06-23 14:53:52 -0700

    Measurements from a indicator sent to ubidots for analysis

---
+++ nodemcu_to_ubidots/nodemcu_to_ubidots.ino
@@ -0,0 +1,15 @@
+#include "Ubidots.h"
+const char* UBIDOTS_TOKEN = "...";  // Put here your Ubidots TOKEN
+const char* WIFI_SSID = "...";      // Put here your Wi-Fi SSID
+const char* WIFI_PASS = "...";      // Put here your Wi-Fi password 
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
